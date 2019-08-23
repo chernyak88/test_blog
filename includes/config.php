ba9c93b2a944167941ac0b4ec2da40d9ecae6ead
chernyak88 <i_chernyak@inbox.ru>
2019-08-23 11:09:51 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array(
+  'title' => 'Блог IT',
+  'vk_url' => 'http://vk.com/somebody',
+  'db' => array(
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'name' => 'test_blog'
+  )
+);
+
+require "db.php";
+
+?>
